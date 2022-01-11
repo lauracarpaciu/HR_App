4f1e561e4deb7bb23af5bd32d94b7e5d007f0382
Mirela <lauracarpaciu>
2022-01-11 07:08:06 +0200

    create an articole num

--- kb/models.py
+++ kb/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
--- kb/views.py
+++ kb/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from .models import Article
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def home(request):
-    context = {'title':'Articles'}
-    return render(request, 'kb/home.html',context)
+    context = {'title': 'Articles', 'articles': Article.objects.all()}
+    return render(request, 'kb/home.html', context)
+
 
 def about(request):
-    # return HttpResponse("<h1> About Knowledge base </h1>")
-    context = {'title':'About'}
-    return render(request, 'kb/about.html',context)
+    # return HttpResponse('<h1> About knowledge base</h1>')
+    context = {'title': 'About KB'}
+    return render(request, 'kb/about.html', context)
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    fields = ['title', 'content']
+    success_url = reverse_lazy('kb-home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
