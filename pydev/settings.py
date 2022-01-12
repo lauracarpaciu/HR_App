a3b55c2dfe14ee0f0ec29fce38f87f41ec9d803f
Mirela <lauracarpaciu>
2022-01-12 20:38:13 +0200

    commit

--- pydev/settings.py
+++ pydev/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'kb.apps.KbConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'kb-home'
+
+LOGIN_URL = 'users-login'
+
--- pydev/urls.py
+++ pydev/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from users import views as users_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', users_views.register, name='users-register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='users-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='users-logout'),
     path('', include('kb.urls'))
 ]
