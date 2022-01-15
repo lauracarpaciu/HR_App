23aaaf121fa32152d4fe2b54380b7d569fdac2fc
Mirela <lauracarpaciu>
2022-01-15 20:07:46 +0200

    migrations and new articles

--- kb/admin.py
+++ kb/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from .models import Reporter
 from .models import Article
 
 # Register your models here.
+admin.site.register(Reporter)
 admin.site.register(Article)
--- kb/models.py
+++ kb/models.py
@@ -3,12 +3,21 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
 
-
-class Article(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=150)
-    content = models.TextField()
-    date = models.DateTimeField(default=timezone.now)
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
 
     def __str__(self):
-        return self.title
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100,default='DEFAULT VALUE')
+    pub_date = models.DateField(default=timezone.now)
+    reporter = models.ForeignKey(Reporter,default='DEFAULT VALUE', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering = ['headline']
