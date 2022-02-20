db642547b2552b67ae67217a0c2fc054d89a3c85
Mirela <lauracarpaciu>
2022-02-20 15:39:29 +0200

    modif apps,forms,models,add signals

--- users/views.py
+++ users/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from users.forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-# Create your views here.
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account for {username} has been created')
-            return redirect('kb-home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
-    @login_required
+@login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
