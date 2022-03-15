b35d3894f22fafb605422e32fc88c9c088fa4d62
Mirela <lauracarpaciu>
2022-03-15 16:47:18 +0200

    admin.py

--- kb/admin.py
+++ kb/admin.py
@@ -7,12 +7,20 @@ from .models import Job
 from .models import Employee
 from .models import Job_History
 
+class JobInline(admin.TabularInline):
+    model = Job
+    extra = 3
+
+class EmployeeAdmin(admin.ModelAdmin):
+    fields = '__all__'
+    inlines = [JobInline]
+
 # Register your models here.
 admin.site.register(Region)
 admin.site.register(Countrie)
 admin.site.register(Location)
 admin.site.register(Department)
 admin.site.register(Job)
-admin.site.register(Employee)
+admin.site.register(Employee,EmployeeAdmin)
 admin.site.register(Job_History)
 
